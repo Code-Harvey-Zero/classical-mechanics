3db3d715165d5c96cff716e28e0b842f1eee1abd
Code-H <harvey.cox@outlook.com>
2026-08-27 10:21:01 +0100

    V.0.3

--- orbital_simulator.py
+++ orbital_simulator.py
@@ -1,35 +1,56 @@
 import numpy as np
+import catppuccin
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 
-#define constants
+#Define constants
 G = 6.67430e-11
 SOLAR_MASS = 1.989e30
 AU = 1.496e11
 DAY_SECONDS = 86400
 DAYS_PER_YEAR = 365.25
+STEPS_PER_DAY = 4
+EARTH_MASS = 5.9722e24
 
-def simulate_orbit(time_period, planet_position, planet_velocity, star_mass):
-    for i in range(int(time_period * DAYS_PER_YEAR)): #A year is 365.25 days how to fix this?
+# Define functions
+def calculate_energy(planet_mass, star_mass, radius, planet_velocity):
+    potential_energy = (- G * star_mass * planet_mass) / radius
+    planet_speed = np.linalg.norm(planet_velocity)
+    kinetic_energy = 0.5 * planet_mass * (planet_speed ** 2)
+    total_energy = kinetic_energy + potential_energy
+    return total_energy
+
+
+def simulate_orbit(time_period, planet_position, planet_velocity, star_mass, planet_mass):
+    for i in range(int(time_period * DAYS_PER_YEAR * STEPS_PER_DAY)): # Computes in quarter days
         planet_x.append(planet_position[0])
         planet_y.append(planet_position[1])
+        planet_velocities.append(np.linalg.norm(planet_velocity))
+        times.append(i * DAY_SECONDS / STEPS_PER_DAY)
 
         radius = np.linalg.norm(planet_position)
-        scalar_acceleration = (star_mass * 6.67 * (10 ** -11)) / (radius ** 2)
+        scalar_acceleration = (star_mass * G / (radius ** 2))
 
         # now find the negative unit vector of the radius squared in order to find the vector acceleration
 
         unit_radius = - planet_position / (radius)
         vector_acceleration = scalar_acceleration * unit_radius
 
+        energy = calculate_energy(planet_mass, star_mass, radius, planet_velocity)
+        energies.append(energy)
         # NOW WE NEED TO FIND THE NEW POSITION AND VELOCITY VECTOR AND MAP THEM INTO VARIABLES AND SPLIT THEM INTO COMPONENT
 
-        planet_velocity += vector_acceleration * DAY_SECONDS
-        planet_position += planet_velocity * DAY_SECONDS
+        planet_velocity += vector_acceleration * (DAY_SECONDS / STEPS_PER_DAY)
+        planet_position += planet_velocity * (DAY_SECONDS / STEPS_PER_DAY)
 
 
+    return planet_x, planet_y, planet_velocities, times, energies
+
+# Check conservation of energy
 
 # first of all get the required data from the planets
 star_mass = int(input('Star Mass (Solar Masses): ')) * SOLAR_MASS
+planet_mass = int(input('Star Mass (Earth Masses): ')) * EARTH_MASS
 
 pos_input = input('Planet Initial Position (x, y) (AU): ')
 planet_position = np.array([float(i) for i in pos_input.split(',')]) * AU
@@ -38,10 +59,31 @@ vel_input = input('Planet Initial Velocity (x, y) (km/s): ')
 planet_velocity = np.array([float(i) for i in vel_input.split(',')]) * 1_000
 
 time_period = int(input('Orbit Period (years): '))
-planet_x, planet_y = [],[]
+planet_x, planet_y, planet_velocities, times, energies = [], [], [], [], []
 
-simulate_orbit(time_period, planet_position, planet_velocity, star_mass)
+simulate_orbit(time_period,
+               planet_position,
+               planet_velocity,
+               star_mass, planet_mass)
 
-plt.plot(planet_x, planet_y)
+mpl.style.use(catppuccin.PALETTE.mocha.identifier)
+
+
+plt.plot(planet_x, planet_y, label = 'Orbit')
+plt.scatter(0,0, label = 'Central body', color = 'orange')
+
+plt.xlabel('x position (m)')
+plt.ylabel('y position (m)')
+plt.axis('equal')
+plt.legend(loc='upper left')
 plt.show()
 
+plt.plot(
+    np.array(times) / (DAY_SECONDS * DAYS_PER_YEAR),
+    np.array(energies) - energies[0]
+)
+
+plt.xlabel('Time (years)')
+plt.ylabel('Change in Energy (J)')
+plt.axhline(0, linestyle='--')
+plt.show()
