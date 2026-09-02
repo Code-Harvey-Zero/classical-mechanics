73fb38711cb9bc20376a9033241fc0977639f2fe
Code-H <harvey.cox@outlook.com>
2026-09-02 08:43:14 +0100

    V1.2.1 # Gui changed, Run button added, energy plot shrunk down and moved

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import tkinter as tk
 import customtkinter as ctk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.animation import FuncAnimation
 
 import catppuccin
 import matplotlib as mpl
@@ -22,6 +21,7 @@ def reset():
     for configure in variable_configurations:
         sld = sliders[configure['name']]
         sld.set(configure['default'])
+    update_simulation()
 
 
 
@@ -49,8 +49,13 @@ def update_simulation():
         return
 
     orbit_line = ax1.plot(planet_x / physics.AU, planet_y / physics.AU, label ='Orbit')
-    ax1.scatter(0,0, label = 'Central body', color = 'orange', s=150)
-    dot = ax1.scatter(planet_x[0] / physics.AU, planet_y[0] / physics.AU, label= 'Planetary body', color = '#00BFFF')
+
+    # Layering from the widest outer glow down to the intense core
+    ax1.scatter([0] * 5, [0] * 5, s=[300, 160, 70, 24, 6],
+                color=['red', 'darkorange', 'orange', 'gold', 'white'],
+                alpha=[0.03, 0.08, 0.2, 0.5, 1.0], edgecolors='none', label='Central Body')
+
+    dot = ax1.scatter(planet_x[0] / physics.AU, planet_y[0] / physics.AU, label= 'Planetary Body', color = '#00BFFF')
 
     ax1.set_xlabel('x position (AU)')
     ax1.set_ylabel('y position (AU)')
@@ -69,7 +74,11 @@ def update_simulation():
     ax2.set_ylabel('Energy Error (%)')
     ax2.set_title('Energy Percentage Error vs Time')
 
-    canvas.draw_idle()
+    orbit_plot.tight_layout()
+    energy_plot.tight_layout()
+
+    orbit_canvas.draw_idle()
+    energy_canvas.draw_idle()
 
 
 # Make Tkinter GUI
@@ -89,65 +98,80 @@ window.minsize(1024,768)
 
 #layout widgets
 background_frame = ctk.CTkFrame(window, fg_color='#1e1e2e')
-menu_frame = ctk.CTkFrame(window, fg_color='#64748B', corner_radius=10)
-main_frame = ctk.CTkFrame(window)
+menu_frame = ctk.CTkTabview(window, fg_color='#64748B', corner_radius=10, border_width=1, border_color = '#CBD5E1', bg_color='#1e1e2e')
+orbit_frame = ctk.CTkFrame(window)
+energy_frame = ctk.CTkFrame(window, border_color='#CBD5E1', border_width=1, corner_radius=5, fg_color='#1e1e2e')
+
+menu_frame.add('Planet')
+menu_frame.add('Settings')
 
 background_frame.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
-menu_frame.place(relx=0.015, rely=0.015, relwidth=0.2, relheight=0.97)
-main_frame.place(relx=0.2, y = 0, relwidth = 0.8, relheight = 1)
+menu_frame.place(relx=0.015, rely=0.01, relwidth=0.3, relheight=0.97)
+orbit_frame.place(relx=0.3, y = 0, relwidth = 0.7, relheight = 1)
+energy_frame.place(relx=0.76, rely=-0.01, relwidth=0.25, relheight=0.26)
 
 menu_frame.lift()
 
-menu_frame.columnconfigure((0), weight = 1)
-menu_frame.rowconfigure(list(range(11)), weight = 1)
+menu_frame.tab('Planet').columnconfigure((0,1), weight = 1)
+menu_frame.tab('Planet').rowconfigure(list(range(5)), weight = 1)
 
 variable_configurations = [
-    {'name':'star_mass', 'label':'Star Mass (Solar Masses):', 'min': 0.1, 'max': 5 , 'default': 1, 'step':980},
-    {'name':'planet_mass', 'label':'Planet Mass (Earth Masses):', 'min': 0.1, 'max': 10, 'default': 1 , 'step':980},
-    {'name':'planet_position', 'label':'Planet x Position (AU):', 'min': 0.1, 'max': 5, 'default': 1 , 'step':980},
-    {'name':'planet_velocity', 'label':'Planet y Velocity (km/s):', 'min': 0 , 'max': 100, 'default': 30, 'step':1000},
-    {'name':'time_period', 'label':'Time Period (years):', 'min':1 , 'max':5 , 'default':1 , 'step':100}]
+    {'name':'star_mass', 'label':'Star Mass (Solar Masses)', 'min': 0.1, 'max': 5 , 'default': 1, 'step':980},
+    {'name':'planet_mass', 'label':'Planet Mass (Earth Masses)', 'min': 0.1, 'max': 10, 'default': 1 , 'step':980},
+    {'name':'planet_position', 'label':'Planet x Position (AU)', 'min': 0.1, 'max': 5, 'default': 1 , 'step':980},
+    {'name':'planet_velocity', 'label':'Planet y Velocity (km/s)', 'min': 0 , 'max': 100, 'default': 30, 'step':1000},
+    {'name':'time_period', 'label':'Time Period (years)', 'min':1 , 'max':5 , 'default':1 , 'step':100}]
 
 variables, sliders = {}, {}
 
-for configure in variable_configurations:
+for i, configure in enumerate(variable_configurations):
+    row_frame = ctk.CTkFrame(master = menu_frame.tab('Planet'), width = 1)
+    row_frame.grid(row=i, column=0, columnspan=2, sticky="ew", padx=10, pady=5)
+    row_frame.columnconfigure(0, weight=4, uniform='row_col')
+    row_frame.columnconfigure(1, weight=4, uniform='row_col')
+    row_frame.columnconfigure(2, weight=1, uniform='row_col')
+
+
+    lbl = ctk.CTkLabel(row_frame,
+                       text=configure['label'],
+                       font=("Helvetica", 14, "bold"),
+                       fg_color='#313244',
+                       border_width=1,
+                       border_color='',
+                       corner_radius=5,
+                       padx=15,
+                       pady=8)
+    lbl.grid(row = 0, column=0, sticky = 'ew', padx = 10, pady = 10)
 
     var = tk.DoubleVar(value=configure['default'])
     variables[configure['name']] = var
 
-    sld = ctk.CTkSlider(menu_frame, from_=configure['min'], to=configure['max'], variable=var,
+    sld = ctk.CTkSlider(row_frame, from_=configure['min'], to=configure['max'], variable=var,
                         number_of_steps=configure['step'])
     sld.set(configure['default'])
+
     sliders[configure['name']] = sld
+    sld.grid(row=0, column=1, sticky='e')
 
-for i, configure in enumerate(variable_configurations):
-    label_row = i * 2
-    control_row = i * 2 + 1
+    box = ctk.CTkEntry(row_frame, width=60, textvariable=var)
+    box.grid(row=0, column=2, sticky='w', padx=10)
+    box.bind("<Return>", lambda event, name=configure["name"]: sliders[name].set(f"{variables[name].get():.3g}"))
 
-    lbl = ctk.CTkLabel(menu_frame, text=configure['label'], font=("Helvetica", 14, "bold"))
-    lbl.grid(row=label_row, column=0 )
+run = ctk.CTkButton(menu_frame.tab('Planet'), text='Run', command = update_simulation)
+run.grid(row=10, column = 1)
 
-    sld = sliders[configure['name']]
-    sld.grid(row=control_row, column=0, sticky = 'n')
+milky_way = ctk.CTkButton(menu_frame.tab('Planet'), text='Reset', command = reset)
+milky_way.grid(row=10, column=0)
 
-    var = variables[configure['name']]
-    box = ctk.CTkEntry(menu_frame, width=60, textvariable=var)
-    box.grid(row=control_row, column=0)
-    box.bind("<Return>", lambda event, name=configure["name"]: sliders[name].set(variables[name].get()))
+orbit_plot, ax1 = plt.subplots()
 
-    var.trace_add("write", lambda *args: update_simulation())
+energy_plot, ax2 = plt.subplots()
 
-milky_way = ctk.CTkButton(menu_frame, text='Reset', command = reset)
-milky_way.grid(row=10)
+orbit_canvas = FigureCanvasTkAgg(orbit_plot, master = orbit_frame)
+orbit_canvas.get_tk_widget().pack(fill='both', expand=True)
 
-fig = plt.figure()
-
-ax1 = fig.add_subplot(1, 2, 1) # ADD projection='3d' to make plot 3d
-ax2 = fig.add_subplot(1,2,2)
-fig.subplots_adjust(wspace=0.4)
-
-canvas = FigureCanvasTkAgg(fig, master = main_frame)
-canvas.get_tk_widget().pack(fill='both', expand=True)
+energy_canvas = FigureCanvasTkAgg(energy_plot, master=energy_frame)
+energy_canvas.get_tk_widget().pack(fill='both', expand=True, padx=10, pady=10)
 
 update_simulation()
 
@@ -156,8 +180,7 @@ window.protocol('WM_DELETE_WINDOW', closing)
 window.mainloop()
 
 # Add sphere orbiting animation
-# Add sphere orbiting animation
-# Make it so enter is required to put 
-# Improve the layout.
+# Add a tickbox for energy error graph, and change energy error graph appearance
+# Make it so enter is required to put input
 # Add validation for impossible/extreme inputs.
 # Then V1 is done and we can move on to V2 which is finding out how a better model for integration
